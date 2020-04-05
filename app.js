3270f82028edfaab180961c642a0d875a752bafc
Alan Croft <alantcroft@gmail.com>
2020-04-05 16:28:24 -0400

    updating
    added enrollment

--- app.js
+++ app.js
@@ -225,6 +225,13 @@ app.get("/myprofile", function(req, res){
   }});
 
 
+  app.get("/enrollment", function(req, res){
+    if(req.isAuthenticated()){
+      res.render("enrollment", {user: req.user})
+    } else {
+      res.redirect("/login")
+    }});
+
 // Authentication requests for Linkedin OAuth
 app.get('/auth/linkedin',
   passport.authenticate('linkedin'),
